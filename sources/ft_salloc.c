9db20e19ec1623241e15e72093b0ef4ee5ed83a2
Luis Geniole <luis.geniole@gmail.com>
2021-04-09 23:16:22 -0300

    Fix norm errors.

--- sources/ft_salloc.c
+++ sources/ft_salloc.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 22:57:20 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2021/04/09 23:12:03 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2021/04/09 23:15:14 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	*ft_salloc(size_t size)
 {
-	void *ptr = malloc(size);
+	void *pointer;
 
-	if (ptr == NULL && (size > 0))
+	pointer = malloc(size);
+	if (pointer == NULL && (size > 0))
 	{
 		perror("Unable to allocate required memory.");
 		exit(EXIT_FAILURE);
 	}
-	return ptr;
+	return (pointer);
 }
