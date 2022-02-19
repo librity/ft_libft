262628d2cdcfc49777a3d38c6314f36f0ccafb4e
Luis Geniole <luis.geniole@gmail.com>
2022-02-19 15:59:39 -0300

    Add printf float examples.

---
+++ examples/ft_printf_f.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_f.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/05 00:24:12 by lpaulo-m          #+#    #+#             */
+/*   Updated: 2022/02/19 15:58:43 by lpaulo-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void	test(float number)
+{
+	int printf_return;
+
+	printf_return = printf("%f\n", number);
+	printf("printf_return: %d\n", printf_return);
+	printf_return = ft_printf("%f\n", number);
+	printf("printf_return: %d\n", printf_return);
+	printf("-----------------------\n");
+}
+
+int	main(void)
+{
+	test(0.000001);
+	test(42.2);
+	test(42.4242);
+	test(-42.4242);
+	test(42.0);
+	test(0.0);
+	test(0.1);
+	test(-0.1);
+	test(1.1);
+	test(-1.1);
+	test(0.000001);
+	test(-0.000001);
+	test(0.002220201);
+	test(-0.002220201);
+	test(42);
+	test('L');
+	test(424242);
+	test(424242424);
+	test(-42.4242);
+
+	return (0);
+}
