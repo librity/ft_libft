3abb873cf7ddfbdf9becaed68d51d767c1d8258b
Luis Geniole <luis.geniole@gmail.com>
2022-02-22 11:48:30 -0300

    Add more examples to ft_atold.

--- examples/ft_atold.c
+++ examples/ft_atold.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 00:24:12 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/02/20 00:55:40 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/02/22 11:31:16 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,6 +125,88 @@ int main(void)
 	printf("--------\n");
 	printf("EXPECTED: %f\n", atof("-1.13821381"));
 	printf("RESULT:   %Lf\n", ft_atold("-1.13821381"));
+	printf("--------\n");
+
+	printf("EXPECTED: %f\n", atof("0.123"));
+	printf("RESULT:   %Lf\n", ft_atold("0.123"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.2321"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.2321"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.3842"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.3842"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.391"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.391"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.4"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.4"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.587"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.587"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.70176"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.70176"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.7269"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.7269"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.75"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.75"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.8"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.8"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-0.835"));
+	printf("RESULT:   %Lf\n", ft_atold("-0.835"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-1.0"));
+	printf("RESULT:   %Lf\n", ft_atold("-1.0"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("-2.0"));
+	printf("RESULT:   %Lf\n", ft_atold("-2.0"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.0"));
+	printf("RESULT:   %Lf\n", ft_atold("0.0"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.01"));
+	printf("RESULT:   %Lf\n", ft_atold("0.01"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.1428"));
+	printf("RESULT:   %Lf\n", ft_atold("0.1428"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.156"));
+	printf("RESULT:   %Lf\n", ft_atold("0.156"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.1889"));
+	printf("RESULT:   %Lf\n", ft_atold("0.1889"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.285"));
+	printf("RESULT:   %Lf\n", ft_atold("0.285"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.3"));
+	printf("RESULT:   %Lf\n", ft_atold("0.3"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.4"));
+	printf("RESULT:   %Lf\n", ft_atold("0.4"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.45"));
+	printf("RESULT:   %Lf\n", ft_atold("0.45"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.6"));
+	printf("RESULT:   %Lf\n", ft_atold("0.6"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("0.745"));
+	printf("RESULT:   %Lf\n", ft_atold("0.745"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("1.0"));
+	printf("RESULT:   %Lf\n", ft_atold("1.0"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("2.0"));
+	printf("RESULT:   %Lf\n", ft_atold("2.0"));
+	printf("--------\n");
+	printf("EXPECTED: %f\n", atof("3.0"));
+	printf("RESULT:   %Lf\n", ft_atold("3.0"));
 	printf("--------\n");
 
 	return (0);
