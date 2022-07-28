6a58e77f75cb21beb59393c06e31b04490277e97
Luis <luis.geniole@gmail.com>
2022-07-28 18:00:18 -0300

    Add documentation comment to ft_die;

--- sources/errors/ft_die.c
+++ sources/errors/ft_die.c
@@ -6,14 +6,17 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 15:42:15 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/07/21 15:43:21 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/07/28 16:54:23 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <internals.h>
 
+/*
+** Prints error message to STDERR_FILENO and exits with status 1.
+*/
 void	ft_die(void *error_message)
 {
-	ft_putstr_fd(error_message, 2);
+	ft_putstr_fd(error_message, STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
