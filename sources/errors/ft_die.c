345681f2c39e97212dda0e37491828c09af48b86
Luis <luis.geniole@gmail.com>
2022-08-01 16:16:31 -0300

    Add endline to ft_die.

--- sources/errors/ft_die.c
+++ sources/errors/ft_die.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 15:42:15 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/07/29 01:10:02 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/08/01 15:17:52 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@
 void	ft_die(void *error_message)
 {
 	ft_putstr_fd(ERROR_PREFIX, STDERR_FILENO);
-	ft_putstr_fd(error_message, STDERR_FILENO);
+	ft_putendl_fd(error_message, STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
