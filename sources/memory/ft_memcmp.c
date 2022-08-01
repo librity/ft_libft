87abad24d27f46d1a40a32595bde0d9455095c5f
Luis <luis.geniole@gmail.com>
2022-08-01 00:19:14 -0300

    Improve ft_memcmp documentation.

--- sources/memory/ft_memcmp.c
+++ sources/memory/ft_memcmp.c
@@ -6,15 +6,15 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 23:59:17 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/03/21 16:51:35 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/07/31 23:23:20 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <internals.h>
 
 /*
-** Compares n bytes after s1 with the n bytes after s2.
-** If any are different it returns the difference.
+** Compares the first n bytes of s1 with the first n bytes of s2.
+** If any one byte is different, returns *s1 - *s2.
 */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
