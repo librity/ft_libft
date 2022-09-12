01cfa45b00cb7fe117b7d2695824610e397fe1db
Luis <luis.geniole@gmail.com>
2022-09-11 23:19:36 -0300

    Improve ft_strarr_cutn.

--- sources/string_arrays/ft_strarr_cutn.c
+++ sources/string_arrays/ft_strarr_cutn.c
@@ -6,27 +6,27 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 17:15:15 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/09/11 19:53:50 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/09/11 23:18:08 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <internals.h>
 
 /**
- * @brief Cuts n pointers of a NULL-terminated C-string array.
+ * @brief Cuts up to n pointers of a NULL-terminated C-string array.
  *
  * @param cut_me The pointer to be cut.
  * @param cut_me The number of pointers to cut.
  */
 void	ft_strarr_cutn(char **cut_me, int n)
 {
+	if (cut_me == NULL)
+		return ;
 	if (n <= 0)
 		return ;
-	if ((size_t)n > ft_strarr_size(cut_me))
-		return ;
-	while (cut_me != NULL && *cut_me != NULL)
+	while (*cut_me != NULL && n > 0)
 	{
-		*cut_me = *(cut_me + n);
-		cut_me++;
+		ft_strarr_cut(cut_me);
+		n--;
 	}
 }
