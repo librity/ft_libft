dfe6a13e1430437f3ba36348ba429d6559b5c800
Luis <luis.geniole@gmail.com>
2022-09-14 12:50:30 -0300

    Add SOF and STX to color escape codes.

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 21:58:19 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/09/13 20:20:20 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/09/14 11:57:55 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -620,27 +620,27 @@ void				ft_clear(void);
 \******************************************************************************/
 
 /* Black, Red, Green, Yellow, Blue, Purple, Cyan, White */
-# define BK "\033[0;30m"
-# define R "\033[0;31m"
-# define G "\033[0;32m"
-# define Y "\033[0;33m"
-# define B "\033[0;34m"
-# define P "\033[0;35m"
-# define C "\033[0;36m"
-# define W "\033[0;37m"
+# define BK "\001\033[0;30m\002"
+# define R "\001\033[0;31m\002"
+# define G "\001\033[0;32m\002"
+# define Y "\001\033[0;33m\002"
+# define B "\001\033[0;34m\002"
+# define P "\001\033[0;35m\002"
+# define C "\001\033[0;36m\002"
+# define W "\001\033[0;37m\002"
 
 /* Bold */
-# define BKB "\033[1;30m"
-# define RB "\033[1;31m"
-# define GB "\033[1;32m"
-# define YB "\033[1;33m"
-# define BB "\033[1;34m"
-# define PB "\033[1;35m"
-# define CB "\033[1;36m"
-# define WB "\033[1;37m"
+# define BKB "\001\033[1;30m\002"
+# define RB "\001\033[1;31m\002"
+# define GB "\001\033[1;32m\002"
+# define YB "\001\033[1;33m\002"
+# define BB "\001\033[1;34m\002"
+# define PB "\001\033[1;35m\002"
+# define CB "\001\033[1;36m\002"
+# define WB "\001\033[1;37m\002"
 
 /* Reset Color */
-# define RC "\033[0m"
+# define RC "\001\033[0m\002"
 
 void				ft_black(char *str);
 void				ft_black_endl(char *str);
